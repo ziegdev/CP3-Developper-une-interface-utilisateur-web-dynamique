fbb549d68db71fde0b17c7a6e08e75f5bae78686
Sébastien Lampazona <sebastien@oclock.io>
2021-03-29 09:34:00 +0200

    Correction beug withROuter

--- src/containers/Recipe/index.js
+++ src/containers/Recipe/index.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import Recipe from 'src/components/Recipe';
+import { withRouter } from 'react-router-dom';
 
 import { findRecipe } from 'src/selectors/recipes';
 
