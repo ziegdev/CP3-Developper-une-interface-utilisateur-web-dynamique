50ca13a13cd5fcd7734560b5dedf683ab94ba599
Sébastien Lampazona <sebastien@oclock.io>
2021-03-30 11:25:47 +0200

    Ajout des tests sur le reducer recipes

---
+++ tests/.setup.js
@@ -0,0 +1,5 @@
+require("@babel/register")();
+require("ignore-styles");
+const enzyme = require("enzyme");
+const Adapter = require("@wojtekmaj/enzyme-adapter-react-17");
+enzyme.configure({ adapter: new Adapter() });
