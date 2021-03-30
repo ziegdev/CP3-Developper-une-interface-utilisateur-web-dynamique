c5bdfa765c000c091896b65b7cb5cdaef09ec4af
Sébastien Lampazona <sebastien@oclock.io>
2021-03-30 11:31:10 +0200

    Ajout du fichier de test de recipes ( mais sans les tests )

---
+++ tests/selectors/recipes.test.js
@@ -0,0 +1,10 @@
+// On test :
+// - La structure comme on l'a fait pour le reducer
+// - On test un retour basique
+// - On teste l'execution
+//   - Un cas qui fait planter la fonction mais du coups qui passe le test
+//   - Au moins 2 cas bien différents qui font passer le test
+
+// Ref la doc de Chai
+// https://www.chaijs.com/api/bdd/
+
