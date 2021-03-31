79f2dd38b369c6c1e2c4118f55565e94c0825d7e
Sébastien Lampazona <sebastien@oclock.io>
2021-03-31 10:57:02 +0200

    Ajout du formulaire de login

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LoginForm from 'src/components/LoginForm';
 import logo from 'src/assets/logo.png';
 import './style.scss';
 
 const AppHeader = () => (
   <header className="header">
     <img src={logo} className="header-logo" alt="Logo oRecipes" />
+    <LoginForm />
   </header>
 );
 
