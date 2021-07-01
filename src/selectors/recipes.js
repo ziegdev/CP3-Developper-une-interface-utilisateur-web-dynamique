c3c42ed896491bb4e7d5d80ec1776c2eec734822
ziegdev <houzetvincent@gmail.com>
2021-07-01 16:30:08 +0200

    test

--- src/selectors/recipes.js
+++ src/selectors/recipes.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable import/prefer-default-export */
 /* eslint-disable arrow-body-style */
 
@@ -7,20 +8,15 @@
  * @param {string} searchedSlug - le slug de la recette recherchée
  * @return {Object} - La recette trouvée
  */
-export function findRecipe(recipes, searchedSlug) {
-<<<<<<< HEAD
-=======
+ export function findRecipe(recipes, searchedSlug) {
   if (!Array.isArray(recipes)) {
     throw new TypeError('recipes parameter is no an array');
   }
->>>>>>> 9dd55619a1507129cd1dc56814dcedca3f4d4c96
   const recipe = recipes.find((testedRecipe) => {
     return testedRecipe.slug === searchedSlug;
   });
   return recipe;
 }
-<<<<<<< HEAD
-=======
 
 export function getTitleFromRecipesNumber(nbRecipes = 0) {
   let title = 'Découvrez toutes nos recettes';
@@ -32,4 +28,3 @@ export function getTitleFromRecipesNumber(nbRecipes = 0) {
   }
   return title;
 }
->>>>>>> 9dd55619a1507129cd1dc56814dcedca3f4d4c96
